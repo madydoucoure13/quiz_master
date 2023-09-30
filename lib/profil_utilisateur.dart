19cf1321c634db7b45dc1022b72870a71cb4c6b8
Almaimoune9 <elmo.almaimoune@odc.local>
2023-09-30 17:26:27 +0000

    Page profil en cours

--- lib/profil_utilisateur.dart
+++ lib/profil_utilisateur.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class ProfilUtilisateur extends StatelessWidget {
@@ -7,16 +6,35 @@ class ProfilUtilisateur extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Second Route'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          child: const Text('Accueil!'),
-        ),
+      // appBar: AppBar(),
+      body: Stack(
+        children: [
+          // Le premier container
+          Container(
+            height: 200,
+            width: 500,
+            margin: const EdgeInsets.only(top: 20, left: 20, right: 20),
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 135, 120, 231),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: IconButton(
+              icon: const Icon(Icons.arrow_back),
+              onPressed: () => Navigator.pop(context),
+            ),
+          ),
+          // Le deuxième container
+          Container(
+            height: 100,
+            width: 300,
+            margin: const EdgeInsets.only(top: 150, left: 50),
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 236, 232, 232),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: const Text('Mon nouveau container'),
+          )
+        ],
       ),
     );
   }
