e95f3bd51d5675e3f54e5e7a23095e060f4a8699
Ibrahim Djingareye <maigaib82@gmail.com>
2023-10-03 18:37:21 +0000

    a terminer at  home

--- lib/quiz_create.dart
+++ lib/quiz_create.dart
@@ -2,34 +2,120 @@ import 'package:flutter/material.dart';
 
 const Color darkBlue = Color.fromARGB(255, 18, 32, 47);
 
-// void main() {
-//   runApp(quiz_create());
-// }
+const List<String> list = <String>["Selectionner une Categorie", 'Two', 'Three', 'Four'];
 
-class quiz_create extends StatelessWidget {
- @override
+class quiz_create extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _quiz_createState();
+}
+
+class _quiz_createState extends State<quiz_create> {
+  String dropdownValue = list.first;
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(right: 16,left: 16,top: 16,bottom: 64),
-        height: MediaQuery.of(context).size.height*0.80,
-        width: MediaQuery.of(context).size.width-32,
-        decoration: BoxDecoration(
- 
-          color: Colors.blue,
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(0),
-              bottomLeft: Radius.circular(MediaQuery.of(context).size.width*.4),
-              bottomRight: const Radius.elliptical(5, 3),
-              topRight: Radius.circular(0)),
-          boxShadow: <BoxShadow>[
-            BoxShadow(
-                color: Colors.grey.withOpacity(1),
-                offset: Offset(2, 2),
-                blurRadius: 10.0),
-          ],
- 
-        ),
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Quiz Create'),
+        actions: [
+          PopupMenuButton<String>(
+            onSelected: (value) {
+              // Ajoutez ici la logique en fonction de l'élément sélectionné.
+            },
+            itemBuilder: (BuildContext context) {
+              return <PopupMenuEntry<String>>[
+                const PopupMenuItem<String>(
+                  value: 'Option 1',
+                  child: Text('Voir profil'),
+                ),
+                const PopupMenuItem<String>(
+                  value: 'Option 2',
+                  child: Text('Modifier profil'),
+                ),
+                const PopupMenuItem<String>(
+                  value: 'Option 3',
+                  child: Text('Supprimer profil'),
+                ),
+                const PopupMenuItem<String>(
+                  value: 'Option 4',
+                  child: Text('A propos de nous'),
+                ),
+                const PopupMenuItem<String>(
+                  value: 'Option 5',
+                  child: Text('Partager l\'appli'),
+                ),
+              ];
+            },
+          ),
+        ],
+      ),
+      body: Stack(
+        children: <Widget>[
+          ClipPath(
+            clipper: Courbe(), // Utilisez votre classe CustomClipPath comme clipper
+            child: Container(
+              height: 100.0, // Ajustez la hauteur selon vos besoins
+              color: Colors.amber, // Couleur de la première barre info user
+            ),
+          ),
+          ClipPath(
+            clipper: Courbe(), // Utilisez votre classe CustomClipPath comme clipper
+            child: Container(
+              height: 92.0, // Ajustez la hauteur selon vos besoins
+              color: Colors.blue, // Couleur de la deuxième barre info user
+            ),
+          ),
+
+          Container(
+            margin: const EdgeInsets.only( top: 120, left: 30,right: 30),
+            padding: const EdgeInsets.only(left :10,top:0, right:0, bottom: 0),
+            width: double.infinity,
+            decoration: BoxDecoration(
+            color: const Color.fromARGB(145, 145, 145, 145),
+              borderRadius: BorderRadius.circular(8)
+            ),
+            child:DropdownButton<String>(
+            value: dropdownValue,
+            onChanged: (String? newValue) {
+              setState(() {
+                dropdownValue = newValue!;
+              });
+            },
+            items: list.map((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          ),
+          )
+        ],
+      ),
     );
   }
+}
+
+class Courbe extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.lineTo(0, size.height);
+    path.quadraticBezierTo(
+      size.width / 4,
+      size.height - 40,
+      size.width / 2,
+      size.height - 20,
+    );
+    path.quadraticBezierTo(
+      3 / 4 * size.width,
+      size.height + 10,
+      size.width,
+      size.height - 20,
+    );
+    path.lineTo(size.width, 0);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
